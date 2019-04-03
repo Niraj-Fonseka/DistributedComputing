6ea5ed171006047c95468bbe1564acda8834adea
MNF4691 <m_n_fonseka@homedepot.com>
2019-04-02 20:31:25 -0500

    SumList base code

---
+++ PararalleProgramming/SumList/SumList.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+class DisListSum {
+    private int [] list;
+    private int low;
+    private int high;
+    private int sum;
+
+    public asum(int [] list , int low , int high){
+        this.list = list;
+        this.low = low;
+        this.high = high
+    }
+
+    public void compute(){
+        if (this.low == this.high) {
+
+        }
+    }
+   
+
+}
+
+public class sumlist {
+
+    public static void main(String[] args) {
+        int [] arr = new int[]{1,2,3,4,5};
+        int low = 0;
+        int high = arr.length -1;
+
+        DisListSum s = new DisListSum(arr , low , high);
+
+
+    }
+}
